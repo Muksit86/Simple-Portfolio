3e386401a7d5dfc14feaf10f2342333fd25b1161
Muksit86 <amuksit7@gmail.com>
2025-02-24 20:52:21 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/Simple-Portfolio/", // Replace with your repository name
+  base: "Simple-Portfolio", // Replace with your repository name
 });
